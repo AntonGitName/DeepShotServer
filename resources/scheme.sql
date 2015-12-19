8b5c76d09f2b871181db3fee69ced4907b953efa
Anton <mordbergak@gmail.com>
2015-12-19 16:45:22 +0300

    Updated bd scheme.

--- resources/scheme.sql
+++ resources/scheme.sql
@@ -1,25 +1,23 @@
-DROP TABLE UserInputRecord;
-DROP TABLE MLOutputRecord;
-DROP TABLE Style;
-
+DROP TABLE IF EXISTS MLOutputRecord;
+DROP TABLE IF EXISTS UserInputRecord;
+DROP TABLE IF EXISTS Style;
 
 CREATE TABLE Style (
-    id              SERIAL      PRIMARY KEY,
+    id              INTEGER      PRIMARY KEY,
     name            TEXT        UNIQUE NOT NULL,
     imageUrl        TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE UserInputRecord (
-    id              SERIAL      PRIMARY KEY,
+    id              INTEGER      PRIMARY KEY,
     username TEXT,
     imageUrl        TEXT UNIQUE NOT NULL,
     style INT REFERENCES Style
 );
 
 CREATE TABLE MLOutputRecord (
-    id              SERIAL      PRIMARY KEY,
+    id              INTEGER      PRIMARY KEY,
     imageUrl        TEXT UNIQUE,
     status TEXT,
-    username TEXT,
     userInputRecord INT REFERENCES UserInputRecord
 );
